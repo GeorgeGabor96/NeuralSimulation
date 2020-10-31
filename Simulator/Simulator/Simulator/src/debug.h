c981a9045062323108f4af54a9f29044d809ef58
GeorgeGabor96 <gaborgeorge96@yahoo.com>
2020-10-31 20:14:37 +0200

    Added initial debug macros and test macros

---
+++ Simulator/Simulator/Simulator/src/debug.h
@@ -0,0 +1,26 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include <stdio.h>
+#include <errno.h>
+
+#ifdef NDEBUG
+#define debug(M, ...)
+#else
+#define debug(M, ...) fprintf(stderr, "[DEBUG] %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#endif // __NDEBUG__
+
+#define errno_text() (errno == 0 ? "None" : strerror(errno))
+
+#define log_error(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, errno_text(), ##__VA_ARGS__)
+
+#define log_warning(M, ...) fprintf(stderr, "[WARNING] (%s:%d: errno: %s) ", M "\n", __FILE__, __LINE__, errno_text(), ##__VA_ARGS__)
+
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##_VA_ARGS__)
+
+#define check(A, M, ...) if (!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+
+#define check_mem(A) check((A), "Out of Memory.")
+
+
+#endif // __DEBUG_H__
