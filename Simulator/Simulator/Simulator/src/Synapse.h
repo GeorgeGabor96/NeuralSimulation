0850925b49b244c63bc552ba1c81282037f24b7a
GeorgeGabor96 <gaborgeorge96@yahoo.com>
2020-11-10 21:50:21 +0200

    added Synapse.h

---
+++ Simulator/Simulator/Simulator/src/Synapse.h
@@ -0,0 +1,9 @@
+#ifndef __SYNAPSE_H__
+#define __SYNAPSE_H__
+
+
+// TO DO: a synapse should have a pointer to a header that defines its class properties
+// TO DO: a synapse should keep in order the spikes it received
+
+
+#endif  // __SYNAPSE_H__
