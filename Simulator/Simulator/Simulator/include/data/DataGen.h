cb995a15a56e1093d72f637417d9c1ad5f78d4ac
GeorgeGabor96 <gaborgeorge96@yahoo.com>
2021-05-05 18:51:16 +0300

    Documented the way Data Generators work

--- Simulator/Simulator/Simulator/include/data/DataGen.h
+++ Simulator/Simulator/Simulator/include/data/DataGen.h
@@ -6,11 +6,17 @@
 
 
 /*************************************************************
-* STRUCTS
+* INTERFACE FOR DATA GENERATION
+* 
+* EACH GENERATOR NEEDS TO FOLLOW THIS INTERFACE TO ALLOW
+* POLYMORPHISM
 *************************************************************/
+
+// The @DataGenerator creates @DataElements, for example it can read a set of images from disk and give the spike train encoding of every image 
 struct DataGenerator;
 typedef struct DataGenerator DataGenerator;
 
+// The @DataElement is a spike train, like random spikes, or spikes that encode something. It has a @time component
 struct DataElement;
 typedef struct DataElement DataElement;
 
@@ -18,21 +24,28 @@ typedef struct DataElement DataElement;
 /*************************************************************
 * DATA GENERATOR FUNCTIONALITY
 *************************************************************/
+// specific data, can be anything
 typedef void __DataGeneratorData;
+
+// check that the specific internal state is correct
 typedef BOOL(*__data_generator_data_is_valid)(__DataGeneratorData* data);
+
+// destroy the internal state
 typedef void(*__data_generator_data_destroy)(__DataGeneratorData* data);
+
+// get the @idx'th @DataElement, like the @idx'th image
 typedef DataElement* (*__data_generator_get_elem)(__DataGeneratorData* data, uint32_t idx);
 
-// MAY NOT NEED THIS TO BE POLYMORPHIC
+
 struct DataGenerator {
-	uint32_t length;							// need to know the number of elements
-	__DataGeneratorData* data;					// for aditional information
+	uint32_t length;							// number of @DataElement's that can be generated
+	__DataGeneratorData* data;					// specific internal state
 	__data_generator_data_is_valid data_is_valid;
 	__data_generator_data_destroy data_destroy;
 	__data_generator_get_elem get_elem;
 };
 
-// common functionality
+// Functionality that is common for each @DataGenerator, those will call the specific functions internally
 BOOL data_generator_is_valid(DataGenerator* generator);
 void data_generator_destroy(DataGenerator* generator);
 DataElement* data_generator_get_element(DataGenerator* generator, uint32_t idx);
@@ -41,16 +54,26 @@ DataElement* data_generator_get_element(DataGenerator* generator, uint32_t idx);
 /*************************************************************
 * DATA ELEMENT FUNCTIONALITY
 *************************************************************/
+
+// specific data, can be anything
 typedef void __DataElementData;
+
+// check if the internal state is ok
 typedef BOOL(*__data_element_data_is_valid)(__DataElementData* data);
+
+// destroys the internal state
 typedef void(*__data_element_data_destroy)(__DataElementData* element);
 
-// get the values for the network input
+// The @DataElement has a @time component to it, this will return the values for a given @time value
+// WARNING: Here I assume that the network is stored in the @__DataElementData structure
 typedef NetworkInputs* (*__data_element_get_values)(DataElement* element, uint32_t time);
-typedef void(*__date_element_remove_values)(DataElement* element, NetworkInputs* values);  // knows how to destroy what __data_element_get_values created
+
+// knows how to destroy what @__data_element_get_values created
+typedef void(*__date_element_remove_values)(DataElement* element, NetworkInputs* values);  
+
 
 typedef struct DataElement {
-	uint32_t duration;						// how much ms
+	uint32_t duration;						// time duration
 	__DataElementData* data;
 	__data_element_data_is_valid data_is_valid;
 	__data_element_data_destroy data_destroy;
@@ -58,16 +81,18 @@ typedef struct DataElement {
 	__date_element_remove_values remove_values;
 }DataElement;
 
-#define data_element_get_values(element, t) ((element) != NULL && (element)->get_values != NULL ? (element)->get_values(element, t) : NULL)
-#define data_element_remove_values(element, values) ((element) != NULL && (element->remove_values) != NULL ? (element)->remove_values(element, values) : 0)
 
-
-// common functionality
+// common functionality, these functions contain the boilerplate code that it is use for every @DataElement
 BOOL data_element_is_valid(DataElement* element);
 void data_element_destroy(DataElement* element);
+NetworkInputs* data_element_get_values(DataElement* element, uint32_t time);
+void data_element_remove_values(DataElement* element, NetworkInputs* values);
 
+
+// helper functions
+
+// general function to destroy a @NetworkInputs structure
 void data_element_base_remove_values(DataElement* element, NetworkInputs* inputs);
-
 
 
 #endif // __DATA_GEN_H__
